446437db787ebef62f70f00df8663484da6f65b1
YALLICO OWNER <yallico2024@gmail.com>
2026-09-01 12:00:02 -0500

    Actualizar _Dezer.js

--- plugins/_Dezer.js
+++ plugins/_Dezer.js
@@ -1,42 +1,17 @@
-
-import axios from 'axios';
-import uploadImage from "../lib/uploadFile.js";
-
-let handler = async (m, { conn, usedPrefix, command, text }) => {
-  let q = m.quoted ? m.quoted : m;
-  let mime = (q.msg || q).mimetype || "";
-  if (!mime) return m.reply("Fotonya Mana?");
-  if (!/image\/(jpe?g|png)/.test(mime))
-    return m.reply(`Tipe ${mime} tidak didukung!`);
-  let ephemeral =
-    conn.chats[m.chat]?.metadata?.ephemeralDuration ||
-    conn.chats[m.chat]?.ephemeralDuration ||
-    false;
-
-  let img = await q.download();
-  let files = await uploadImage(img);
-  let removebg = await axios.get(`https://widipe.com/removebg?url=${files}`)
-
-
-      try {
-        let out = removebg.data.result.urls
-        await conn.sendMessage(
-          m.chat,
-          {
-            image: { url: out },
-            fileName: "removebg.png",
-            mimetype: "image/png",
-            caption: "*DONE (≧ω≦)ゞ*",
-          },
-          { quoted: m, ephemeralExpiration: ephemeral },
-        );
-      } catch (e) {
-        console.error(e);
-        m.reply('Terjadi kesalahan saat menghapus latar belakang.');
-      }
+import uploadImage from '../lib/uploadImage.js';
+import {sticker} from '../lib/sticker.js';
+const handler = async (m, {conn, text}) => {
+  try {
+    const q = m.quoted ? m.quoted : m;
+    const mime = (q.msg || q).mimetype || '';
+    const img = await q.download();
+    const url = await uploadImage(img);
+    const sremovebg = global.API(`https://api.lolhuman.xyz/api/removebg?apikey=${lolkeysapi}&img=${url}`);
+    const stickerr = await sticker(false, sremovebg, global.packname, global.author);
+    conn.sendFile(m.chat, stickerr, 'sticker.webp', '', m, {asSticker: true});
+  } catch (e) {
+    m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾 𝚂𝙸𝙴𝙽𝚃𝙾, 𝙾𝙲𝚄𝚁𝚁𝙸𝙾 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁, 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾, 𝙽𝙾 𝙾𝙻𝚅𝙸𝙳𝙴 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴𝚁 𝙰 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝙻𝙰 𝙲𝚄𝙰𝙻 𝚂𝙴 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝚁𝙰 𝙴𝙽 𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝚂𝙸𝙽 𝙵𝙾𝙽𝙳𝙾*');
+  }
 };
-handler.help = ["removebg", "changebg"];
-handler.tags = ["tools", "premium"];
-handler.command = /^(removebg|changebg)$/i;
-handler.premium = false; handler.error = 0
+handler.command = /^sremovebg|removebg$/i;
 export default handler;
