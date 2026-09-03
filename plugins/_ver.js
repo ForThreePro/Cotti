1655f660d9ae62098f84532dcc30f011b76e758e
YALLICO OWNER <yallico2024@gmail.com>
2026-09-03 13:04:43 -0500

    Actualizar _ver.js

--- plugins/_ver.js
+++ plugins/_ver.js
@@ -1,17 +1,16 @@
 let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
 
 let handler = async (m, { conn }) => {
-if (!m.quoted) return conn.reply(m.chat, `《✧》 Responde a una imagen ViewOnce.`, m)
-if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `《✧》 Responde a una imagen ViewOnce.`, m)
+if (!m.quoted) return conn.reply(m.chat, `🐱 *COTTI BOTS* \n🌸 Marie dice: Responde a una imagen ViewOnce~`, m)
+if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `🐱 *COTTI BOTS* \n🌸 Marie dice: Eso no es una imagen ViewOnce~`, m)
 let buffer = await m.quoted.download(false);
 if (/videoMessage/.test(m.quoted.mtype)) {
-return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', m)
+return conn.sendFile(m.chat, buffer, 'COTTI_BOTS_Marie.mp4', m.quoted.caption || '', m)
 } else if (/imageMessage/.test(m.quoted.mtype)) {
-return conn.sendFile(m.chat, buffer, 'media.jpg', m.quoted?.caption || '', m)
+return conn.sendFile(m.chat, buffer, 'COTTI_BOTS_Marie.jpg', m.quoted?.caption || '', m)
 }}
 handler.help = ['ver']
 handler.tags = ['tools']
 handler.command = ['readviewonce', 'read', 'ver'] 
-
 
 export default handler
