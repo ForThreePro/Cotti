822d4a347a5a5dab958c4b15f746081edb41056c
YALLICO OWNER <yallico2024@gmail.com>
2026-09-03 13:22:44 -0500

    Actualizar group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -1,7 +1,9 @@
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
     let mentionedJid = m.mentionedJid && m.mentionedJid[0]? m.mentionedJid[0] : m.quoted? m.quoted.sender : null
 
-    if (!mentionedJid) return conn.reply(m.chat, `🐱 𓆩 ***𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🐱
+    if (!mentionedJid) return conn.reply(m.chat, `🐱 𓆩 ***COTTI BOTS OFICIAL*** 𓆪 🐱
+
+🌸 *Marie dice: Así me usas*
 
 *Uso:*
 .${command} @user → Para expulsar
@@ -17,26 +19,26 @@ let handler = async (m, { conn, participants, usedPrefix, command }) => {
         let user = participants.find(p => p.id === mentionedJid)
         let isAdmin = user?.admin
 
-        if (mentionedJid === conn.user.jid) return conn.reply(m.chat, `🐱 *No puedo eliminarme a mí mismo.*`, m)
-        if (mentionedJid === ownerGroup) return conn.reply(m.chat, `🍕 *No puedo eliminar al propietario del grupo.*`, m)
-        if (mentionedJid === ownerBot) return conn.reply(m.chat, `😼 *No puedo eliminar al dueño del bot.*`, m)
-        if (isAdmin) return conn.reply(m.chat, `🍕 *No puedo expulsar a un administrador.*`, m)
+        if (mentionedJid === conn.user.jid) return conn.reply(m.chat, `🐱 *Marie dice: No puedo eliminarme a mí misma.*`, m)
+        if (mentionedJid === ownerGroup) return conn.reply(m.chat, `💖 *Marie dice: No puedo eliminar al propietario del grupo.*`, m)
+        if (mentionedJid === ownerBot) return conn.reply(m.chat, `😿 *Marie dice: No puedo eliminar al dueño del bot.*`, m)
+        if (isAdmin) return conn.reply(m.chat, `💖 *Marie dice: No puedo expulsar a un administrador.*`, m)
 
         await m.react('👢')
         await conn.groupParticipantsUpdate(m.chat, [mentionedJid], 'remove')
 
         conn.reply(m.chat, `🐱 𓆩 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 𝗘𝗫𝗣𝗨𝗟𝗦𝗔𝗗𝗢 𓆪 🐱
 
-.⃟𖥔 ݁. 𖦹˙— \`\`KICK\`\` —˙𖦹.🍕꒷
+.⃟𖥔 ݁. 𖦹˙— \`\`KICK\`\` —˙𖦹.💖꒷
 
 👢 *Usuario:* @${mentionedJid.split('@')[0]}
 👑 *Por:* @${m.sender.split('@')[0]}
 
 ━━━━━━━━━━━
-*Powered by*: ***Garfield Bot Oficial*** 🍕`, m, { mentions: [mentionedJid, m.sender] })
+*Powered by*: ***COTTI BOTS x Marie*** 🌸`, m, { mentions: [mentionedJid, m.sender] })
     } catch (e) {
         await m.react('❌')
-        conn.reply(m.chat, `❌ *Se ha producido un problema.*\n> *Error:* ${e.message}`, m)
+        conn.reply(m.chat, `😿 *Marie dice: Se ha producido un problema.*\n> *Error:* ${e.message}`, m)
     }
 }
 
