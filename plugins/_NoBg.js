f126fe0948d239332d923a50a5b8572c8aedc74b
YALLICO OWNER <yallico2024@gmail.com>
2026-09-07 16:14:55 -0500

    Actualizar _NoBg.js

--- plugins/_NoBg.js
+++ plugins/_NoBg.js
@@ -83,6 +83,6 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 }
 
 handler.help = ['removebg', 'rbg', 'nobg']
-handler.tags = ['tools', 'ai']
+handler.tags = ['tools']
 handler.command = /^(removebg|rbg|nobg)$/i
 export default handler
